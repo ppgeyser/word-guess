25244f495469b5bcf75b3e89ac9a7cfa108ff5f0
Peter Geyser <peterpgeyser@gmail.com>
2019-07-09 15:46:05 -0700

    Adding some variables to game.js in order to flesh out pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,40 @@ $(document).ready(function () {
 
     $("#content").hide();
 
-    var computerChoices = [
-        "beef wellington",
-        "herb omelette",
-        "lamb sauce",
-        "lobster tortellini",
-        "toffee pudding",
-        "stuffed pork tenderloin",
-        "english garden salad"
-        ];
-
-    var chances = 10;
-    var wins = 0;
-    var losses = 0;
-    var guesses = [];
-
     $("#intro-button").click(function(){
         $("#intro").hide();
         $("#content").show();
 
+        var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+        var computerChoices = [
+            "beef wellington",
+            "herb omelette",
+            "lamb sauce",
+            "lobster tortellini",
+            "toffee pudding",
+            "stuffed pork tenderloin",
+            "english garden salad"
+            ];
+    
+        var chances = 10;
+        var wins = 0;
+        var losses = 0;
+        var guesses = [];
+
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log("Computer guess: " + computerGuess);
+
+        $('#chances-text').text(chances);
+        $('#wins-text').text(wins);
+        $('#losses-text').text(losses);
+        $('#guesses-text').text(guesses);
+
         //reset function
-            //select new random string
-            //chances restored to 10
+            //computer selects random string from computerChoices array
+            //print blanks that correspond to string length of chosen string
+                //insert spaces where applicable. spaces should not count as characters that users must guess
+            //var chances restored to 10
             //empty guesses array
             //show content div
 
@@ -40,10 +52,6 @@ $(document).ready(function () {
             //show idiot sandwich gif and play again button
             //play again button onclick
                 //reset function
-
-        //computer selects random string from computerChoices array
-        //print blanks that correspond to string length of chosen string
-            //insert spaces where applicable. spaces should not count as characters that users must guess
         
         //onkeyup function
             //if letter has not been guessed and is an actual letter
